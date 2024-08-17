b69551ed0d1de1cc68e4aa3005fa456f16fc8244
Kevin Baugh <loraxx753@gmail.com>
2024-08-17 16:07:18 -0500

    Changed vitecofig to fix gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/madroxjs/',  // Set this to your repository name
   plugins: [
     react({
       include: /\.(jsx|js|mdx|md|tsx|ts)$/,
