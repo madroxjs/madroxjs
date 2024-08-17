f94a8911ec3a5a6d7df90d0119ed8088a70d9382
loraxx753 <loraxx753@users.noreply.github.com>
2024-08-17 22:10:50 +0000

    deploy: fdad8fa310271de3cb35fcf18f63253f461b1576

--- src/components/Atoms/Atom/component.tsx
+++
@@ -1,8 +0,0 @@
-// import { HTMLAttributes } from "react"
-
-
-// type AtomProps = HTMLAttributes<HTMLDivElement>
-
-export const Atom = () => <div>Hello</div>
-
-Atom.displayName = "Atom"
--- src/queries/index.ts
+++
@@ -1,5 +0,0 @@
-import * as GET from './GET'
-
-export {
-    GET
-}
