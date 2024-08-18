e9ac95241e0099c53795515d4dc32c2c044702c0
loraxx753 <loraxx753@users.noreply.github.com>
2024-08-18 23:08:20 +0000

    deploy: 6bbd95b887e0f70e424e06a73febb786e9165645

---
+++ docs/assets/Layout-CNUPHzCi.js
@@ -0,0 +1,12 @@
+import{j as i}from"./jsx-runtime-DBEcOq3S.js";import{useMDXComponents as n}from"./index-CA_HyBnq.js";import{ae as o}from"./index-CQGfXO-m.js";import"./index-DtJulBIN.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-W4cvx9L_.js";import"../sb-preview/runtime.js";import"./index-BqTDQIm7.js";import"./index-D-8MO0q_.js";import"./extends-CF3RwP-h.js";import"./_defineProperty-DSxlPr5D.js";import"./_overArg-BZNRQ6_9.js";import"./_baseAssignValue-DD5bxUnJ.js";import"./_getPrototype-By7W0WFf.js";import"./index-CFFTH5x_.js";import"./index-DrFu-skq.js";function s(t){const e={code:"code",h2:"h2",h3:"h3",...n(),...t.components};return i.jsxs(i.Fragment,{children:[i.jsx(o,{title:"Documentation/Lib Folder/Layout"}),`
+`,i.jsx(e.h2,{id:"folders",children:"Folders"}),`
+`,i.jsxs(e.h3,{id:"storybook-blocks-blocks",children:["Storybook Blocks ",i.jsx(e.code,{children:"blocks/"})]}),`
+`,i.jsxs(e.h3,{id:"markdown-documentation-documentation",children:["Markdown Documentation ",i.jsx(e.code,{children:"documentation/"})]}),`
+`,i.jsxs(e.h3,{id:"graphql-queries-queries",children:["Graphql Queries ",i.jsx(e.code,{children:"queries/"})]}),`
+`,i.jsxs(e.h3,{id:"graphql-schemas-schemas",children:["Graphql Schemas ",i.jsx(e.code,{children:"schemas/"})]}),`
+`,i.jsxs(e.h3,{id:"tailwind-customization-tailwind",children:["Tailwind Customization ",i.jsx(e.code,{children:"tailwind/"})]}),`
+`,i.jsxs(e.h3,{id:"typescript-types-types",children:["Typescript Types ",i.jsx(e.code,{children:"types/"})]}),`
+`,i.jsx(e.h2,{id:"files",children:"Files"}),`
+`,i.jsxs(e.h3,{id:"graphql-client-clienttsx",children:["GraphQL Client ",i.jsx(e.code,{children:"client.tsx"})]}),`
+`,i.jsxs(e.h3,{id:"dummy-data-dbjson",children:["Dummy Data ",i.jsx(e.code,{children:"db.json"})]}),`
+`,i.jsxs(e.h3,{id:"utilities-utilstsx",children:["Utilities ",i.jsx(e.code,{children:"utils.tsx"})]})]})}function w(t={}){const{wrapper:e}={...n(),...t.components};return e?i.jsx(e,{...t,children:i.jsx(s,{...t})}):s(t)}export{w as default};
---
+++ docs/assets/sizing-Np857r1e.js
@@ -0,0 +1,216 @@
+import{j as n}from"./jsx-runtime-DBEcOq3S.js";import{useMDXComponents as d}from"./index-CA_HyBnq.js";import{ae as l,af as t}from"./index-CQGfXO-m.js";import{Documentation as s,SourceCode as h}from"./sizing-Bg7q5_Yf.js";import"./index-DtJulBIN.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-W4cvx9L_.js";import"../sb-preview/runtime.js";import"./index-BqTDQIm7.js";import"./index-D-8MO0q_.js";import"./extends-CF3RwP-h.js";import"./_defineProperty-DSxlPr5D.js";import"./_overArg-BZNRQ6_9.js";import"./_baseAssignValue-DD5bxUnJ.js";import"./_getPrototype-By7W0WFf.js";import"./index-CFFTH5x_.js";import"./index-DrFu-skq.js";const c={height:({theme:i})=>({auto:"auto",...i("spacing"),"1/2":"50%","1/3":"33.333333%","2/3":"66.666667%","1/4":"25%","2/4":"50%","3/4":"75%","1/5":"20%","2/5":"40%","3/5":"60%","4/5":"80%","1/6":"16.666667%","2/6":"33.333333%","3/6":"50%","4/6":"66.666667%","5/6":"83.333333%",full:"100%",screen:"100vh",svh:"100svh",lvh:"100lvh",dvh:"100dvh",min:"min-content",max:"max-content",fit:"fit-content"}),maxHeight:({theme:i})=>({...i("spacing"),none:"none",full:"100%",screen:"100vh",svh:"100svh",lvh:"100lvh",dvh:"100dvh",min:"min-content",max:"max-content",fit:"fit-content"}),maxWidth:({theme:i,breakpoints:e})=>({...i("spacing"),none:"none",xs:"20rem",sm:"24rem",md:"28rem",lg:"32rem",xl:"36rem","2xl":"42rem","3xl":"48rem","4xl":"56rem","5xl":"64rem","6xl":"72rem","7xl":"80rem",full:"100%",min:"min-content",max:"max-content",fit:"fit-content",prose:"65ch",...e(i("screens"))}),minHeight:({theme:i})=>({...i("spacing"),full:"100%",screen:"100vh",svh:"100svh",lvh:"100lvh",dvh:"100dvh",min:"min-content",max:"max-content",fit:"fit-content"}),minWidth:({theme:i})=>({...i("spacing"),full:"100%",min:"min-content",max:"max-content",fit:"fit-content"}),size:({theme:i})=>({auto:"auto",...i("spacing"),"1/2":"50%","1/3":"33.333333%","2/3":"66.666667%","1/4":"25%","2/4":"50%","3/4":"75%","1/5":"20%","2/5":"40%","3/5":"60%","4/5":"80%","1/6":"16.666667%","2/6":"33.333333%","3/6":"50%","4/6":"66.666667%","5/6":"83.333333%","1/12":"8.333333%","2/12":"16.666667%","3/12":"25%","4/12":"33.333333%","5/12":"41.666667%","6/12":"50%","7/12":"58.333333%","8/12":"66.666667%","9/12":"75%","10/12":"83.333333%","11/12":"91.666667%",full:"100%",min:"min-content",max:"max-content",fit:"fit-content"}),width:({theme:i})=>({auto:"auto",...i("spacing"),"1/2":"50%","1/3":"33.333333%","2/3":"66.666667%","1/4":"25%","2/4":"50%","3/4":"75%","1/5":"20%","2/5":"40%","3/5":"60%","4/5":"80%","1/6":"16.666667%","2/6":"33.333333%","3/6":"50%","4/6":"66.666667%","5/6":"83.333333%","1/12":"8.333333%","2/12":"16.666667%","3/12":"25%","4/12":"33.333333%","5/12":"41.666667%","6/12":"50%","7/12":"58.333333%","8/12":"66.666667%","9/12":"75%","10/12":"83.333333%","11/12":"91.666667%",full:"100%",screen:"100vw",svw:"100svw",lvw:"100lvw",dvw:"100dvw",min:"min-content",max:"max-content",fit:"fit-content"})},m=`export default {
+    height: ({ theme }) => ({
+      auto: 'auto',
+      ...theme('spacing'),
+      '1/2': '50%',
+      '1/3': '33.333333%',
+      '2/3': '66.666667%',
+      '1/4': '25%',
+      '2/4': '50%',
+      '3/4': '75%',
+      '1/5': '20%',
+      '2/5': '40%',
+      '3/5': '60%',
+      '4/5': '80%',
+      '1/6': '16.666667%',
+      '2/6': '33.333333%',
+      '3/6': '50%',
+      '4/6': '66.666667%',
+      '5/6': '83.333333%',
+      full: '100%',
+      screen: '100vh',
+      svh: '100svh',
+      lvh: '100lvh',
+      dvh: '100dvh',
+      min: 'min-content',
+      max: 'max-content',
+      fit: 'fit-content',
+  }),
+  maxHeight: ({ theme }) => ({
+      ...theme('spacing'),
+      none: 'none',
+      full: '100%',
+      screen: '100vh',
+      svh: '100svh',
+      lvh: '100lvh',
+      dvh: '100dvh',
+      min: 'min-content',
+      max: 'max-content',
+      fit: 'fit-content',
+  }),
+  maxWidth: ({ theme, breakpoints }) => ({
+    ...theme('spacing'),
+    none: 'none',
+    xs: '20rem',
+    sm: '24rem',
+    md: '28rem',
+    lg: '32rem',
+    xl: '36rem',
+    '2xl': '42rem',
+    '3xl': '48rem',
+    '4xl': '56rem',
+    '5xl': '64rem',
+    '6xl': '72rem',
+    '7xl': '80rem',
+    full: '100%',
+    min: 'min-content',
+    max: 'max-content',
+    fit: 'fit-content',
+    prose: '65ch',
+    ...breakpoints(theme('screens')),
+  }),
+  minHeight: ({ theme }) => ({
+    ...theme('spacing'),
+    full: '100%',
+    screen: '100vh',
+    svh: '100svh',
+    lvh: '100lvh',
+    dvh: '100dvh',
+    min: 'min-content',
+    max: 'max-content',
+    fit: 'fit-content',
+  }),
+  minWidth: ({ theme }) => ({
+    ...theme('spacing'),
+    full: '100%',
+    min: 'min-content',
+    max: 'max-content',
+    fit: 'fit-content',
+  }),
+  size: ({ theme }) => ({
+    auto: 'auto',
+    ...theme('spacing'),
+    '1/2': '50%',
+    '1/3': '33.333333%',
+    '2/3': '66.666667%',
+    '1/4': '25%',
+    '2/4': '50%',
+    '3/4': '75%',
+    '1/5': '20%',
+    '2/5': '40%',
+    '3/5': '60%',
+    '4/5': '80%',
+    '1/6': '16.666667%',
+    '2/6': '33.333333%',
+    '3/6': '50%',
+    '4/6': '66.666667%',
+    '5/6': '83.333333%',
+    '1/12': '8.333333%',
+    '2/12': '16.666667%',
+    '3/12': '25%',
+    '4/12': '33.333333%',
+    '5/12': '41.666667%',
+    '6/12': '50%',
+    '7/12': '58.333333%',
+    '8/12': '66.666667%',
+    '9/12': '75%',
+    '10/12': '83.333333%',
+    '11/12': '91.666667%',
+    full: '100%',
+    min: 'min-content',
+    max: 'max-content',
+    fit: 'fit-content',
+  }),
+  width: ({ theme }) => ({
+    auto: 'auto',
+    ...theme('spacing'),
+    '1/2': '50%',
+    '1/3': '33.333333%',
+    '2/3': '66.666667%',
+    '1/4': '25%',
+    '2/4': '50%',
+    '3/4': '75%',
+    '1/5': '20%',
+    '2/5': '40%',
+    '3/5': '60%',
+    '4/5': '80%',
+    '1/6': '16.666667%',
+    '2/6': '33.333333%',
+    '3/6': '50%',
+    '4/6': '66.666667%',
+    '5/6': '83.333333%',
+    '1/12': '8.333333%',
+    '2/12': '16.666667%',
+    '3/12': '25%',
+    '4/12': '33.333333%',
+    '5/12': '41.666667%',
+    '6/12': '50%',
+    '7/12': '58.333333%',
+    '8/12': '66.666667%',
+    '9/12': '75%',
+    '10/12': '83.333333%',
+    '11/12': '91.666667%',
+    full: '100%',
+    screen: '100vw',
+    svw: '100svw',
+    lvw: '100lvw',
+    dvw: '100dvw',
+    min: 'min-content',
+    max: 'max-content',
+    fit: 'fit-content',
+  }),
+
+}`;function o(i){const e={a:"a",code:"code",h1:"h1",h2:"h2",h3:"h3",li:"li",p:"p",ul:"ul",...d(),...i.components};return n.jsxs(n.Fragment,{children:[n.jsx(l,{title:"Tailwind/Theme/Sizing"}),`
+`,n.jsxs(e.h1,{id:"sizing-jump-to-source",children:[n.jsx(e.a,{href:"https://tailwindcss.com/docs/width",rel:"nofollow",children:"Sizing"})," ",n.jsx("small",{className:"text-xs",children:n.jsx(e.a,{href:"#source-code",children:"Jump to source"})})]}),`
+`,n.jsxs(e.p,{children:["The ",n.jsx(e.code,{children:"sizing"})," property includes utilities for managing the size of elements, including height, width, and maximum and minimum sizes."]}),`
+`,n.jsx(e.h2,{id:"height",children:n.jsx(e.code,{children:"height"})}),`
+`,n.jsx(e.p,{children:"Defines the height of elements with various options."}),`
+`,n.jsx(t,{language:"tsx",code:`height: ${c.height}`}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[n.jsx(e.code,{children:"auto"}),": Default height value."]}),`
+`,n.jsxs(e.li,{children:["Percentage-based values (e.g., ",n.jsx(e.code,{children:"50%"}),", ",n.jsx(e.code,{children:"33.333333%"}),"): Specifies the height relative to the containing block."]}),`
+`,n.jsxs(e.li,{children:["Fixed values (e.g., ",n.jsx(e.code,{children:"100vh"}),", ",n.jsx(e.code,{children:"100svh"}),", etc.): Defines the height based on viewport units or content size."]}),`
+`]}),`
+`,n.jsx(e.h2,{id:"maxheight",children:n.jsx(e.code,{children:"maxHeight"})}),`
+`,n.jsx(e.p,{children:"Sets the maximum height of elements."}),`
+`,n.jsx(t,{language:"tsx",code:`maxHeight: ${c.maxHeight}`}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[n.jsx(e.code,{children:"none"}),": No maximum height."]}),`
+`,n.jsxs(e.li,{children:["Full height (e.g., ",n.jsx(e.code,{children:"100%"}),", ",n.jsx(e.code,{children:"100vh"}),", etc.): Defines the maximum height."]}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.code,{children:"min-content"}),", ",n.jsx(e.code,{children:"max-content"}),", ",n.jsx(e.code,{children:"fit-content"}),": Sizes based on content."]}),`
+`]}),`
+`,n.jsx(e.h2,{id:"maxwidth",children:n.jsx(e.code,{children:"maxWidth"})}),`
+`,n.jsx(e.p,{children:"Sets the maximum width of elements."}),`
+`,n.jsx(t,{language:"tsx",code:`maxWidth: ${c.maxWidth}`}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[n.jsx(e.code,{children:"none"}),": No maximum width."]}),`
+`,n.jsxs(e.li,{children:["Fixed values (e.g., ",n.jsx(e.code,{children:"20rem"}),", ",n.jsx(e.code,{children:"24rem"}),", etc.): Defines the maximum width."]}),`
+`,n.jsxs(e.li,{children:["Percentage-based values (",n.jsx(e.code,{children:"100%"}),"), content-based values (",n.jsx(e.code,{children:"min-content"}),", ",n.jsx(e.code,{children:"max-content"}),", ",n.jsx(e.code,{children:"fit-content"}),")."]}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.code,{children:"prose"}),": A special value for content width (65ch)."]}),`
+`]}),`
+`,n.jsx(e.h2,{id:"minheight",children:n.jsx(e.code,{children:"minHeight"})}),`
+`,n.jsx(e.p,{children:"Defines the minimum height of elements."}),`
+`,n.jsx(t,{language:"tsx",code:`minHeight: ${c.minHeight}`}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[n.jsx(e.code,{children:"full"}),", ",n.jsx(e.code,{children:"screen"}),", ",n.jsx(e.code,{children:"svh"}),", ",n.jsx(e.code,{children:"lvh"}),", ",n.jsx(e.code,{children:"dvh"}),": Specifies minimum height based on viewport or content size."]}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.code,{children:"min-content"}),", ",n.jsx(e.code,{children:"max-content"}),", ",n.jsx(e.code,{children:"fit-content"}),": Sizes based on content."]}),`
+`]}),`
+`,n.jsx(e.h2,{id:"minwidth",children:n.jsx(e.code,{children:"minWidth"})}),`
+`,n.jsx(e.p,{children:"Defines the minimum width of elements."}),`
+`,n.jsx(t,{language:"tsx",code:`minWidth: ${c.minWidth}`}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[n.jsx(e.code,{children:"full"}),": Full width."]}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.code,{children:"min-content"}),", ",n.jsx(e.code,{children:"max-content"}),", ",n.jsx(e.code,{children:"fit-content"}),": Sizes based on content."]}),`
+`]}),`
+`,n.jsx(e.h2,{id:"size",children:n.jsx(e.code,{children:"size"})}),`
+`,n.jsx(e.p,{children:"Defines the size of elements."}),`
+`,n.jsx(t,{language:"tsx",code:`size: ${c.size}`}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[n.jsx(e.code,{children:"auto"}),": Default size value."]}),`
+`,n.jsxs(e.li,{children:["Percentage-based values (e.g., ",n.jsx(e.code,{children:"50%"}),", ",n.jsx(e.code,{children:"33.333333%"}),"): Specifies the size relative to the containing block."]}),`
+`,n.jsxs(e.li,{children:["Fixed values (",n.jsx(e.code,{children:"100%"}),", ",n.jsx(e.code,{children:"min-content"}),", ",n.jsx(e.code,{children:"max-content"}),", ",n.jsx(e.code,{children:"fit-content"}),")."]}),`
+`,n.jsxs(e.li,{children:["Fractional units (e.g., ",n.jsx(e.code,{children:"1/12"}),", ",n.jsx(e.code,{children:"2/12"}),"): Defines size in a 12-column grid system."]}),`
+`]}),`
+`,n.jsx(e.h2,{id:"width",children:n.jsx(e.code,{children:"width"})}),`
+`,n.jsx(e.p,{children:"Sets the width of elements."}),`
+`,n.jsx(t,{language:"tsx",code:`width: ${c.width}`}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[n.jsx(e.code,{children:"auto"}),": Default width value."]}),`
+`,n.jsxs(e.li,{children:["Percentage-based values (e.g., ",n.jsx(e.code,{children:"50%"}),", ",n.jsx(e.code,{children:"33.333333%"}),"): Specifies the width relative to the containing block."]}),`
+`,n.jsxs(e.li,{children:["Fixed values (",n.jsx(e.code,{children:"100vw"}),", ",n.jsx(e.code,{children:"100svw"}),", etc.): Defines width based on viewport units or content size."]}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.code,{children:"min-content"}),", ",n.jsx(e.code,{children:"max-content"}),", ",n.jsx(e.code,{children:"fit-content"}),": Sizes based on content."]}),`
+`]}),`
+`,typeof s=="function"&&s().toString()!==""&&n.jsxs(n.Fragment,{children:[n.jsx(e.h2,{children:"Extended Documentation"}),n.jsx(s,{})]}),`
+`,n.jsx(e.h2,{id:"source-code",children:"Source Code"}),`
+`,n.jsx(t,{language:"js",code:m}),`
+`,typeof h=="function"&&h().toString()!==""&&n.jsxs(n.Fragment,{children:[n.jsx(e.h3,{children:"Extended Source Code"}),n.jsx(h,{})]})]})}function $(i={}){const{wrapper:e}={...d(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(o,{...i})}):o(i)}export{$ as default};
