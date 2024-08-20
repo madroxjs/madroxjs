586e7accda956ee3f88097ceefa65fcca4d6b015
loraxx753 <loraxx753@users.noreply.github.com>
2024-08-20 03:17:41 +0000

    deploy: 678b77c5ed1a157e0554848150e40a904b39642d

---
+++ docs/assets/grid-YV0yCmRf.js
@@ -0,0 +1,5 @@
+import{j as t}from"./jsx-runtime-DBEcOq3S.js";import{useMDXComponents as r}from"./index-CA_HyBnq.js";import{af as i,ae as m}from"./index-Spl-2Ge4.js";import"./index-DtJulBIN.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-2QVdouCu.js";import"../sb-preview/runtime.js";import"./index-BqTDQIm7.js";import"./index-D-8MO0q_.js";import"./extends-CF3RwP-h.js";import"./_defineProperty-DSxlPr5D.js";import"./_overArg-BZNRQ6_9.js";import"./_baseAssignValue-DD5bxUnJ.js";import"./_getPrototype-By7W0WFf.js";import"./index-CFFTH5x_.js";import"./index-DrFu-skq.js";const s="export default {}";function c(){return t.jsx(i,{language:"tsx",code:s})}function d(){return""}function n(e){const o={h1:"h1",h2:"h2",...r(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(o.h1,{id:"extended-grid",children:"Extended Grid"}),`
+`,t.jsx(m,{isTemplate:!0,title:"Tailwind/Theme/Extended/Grid"}),`
+`,t.jsx(d,{}),`
+`,t.jsx(o.h2,{id:"source-code",children:"Source Code"}),`
+`,t.jsx(c,{})]})}function w(e={}){const{wrapper:o}={...r(),...e.components};return o?t.jsx(o,{...e,children:t.jsx(n,{...e})}):n(e)}export{d as Documentation,c as SourceCode,w as default};
